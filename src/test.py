bfe5ec3e73f9b8eeb8f52bdcc572658d52a098fa
Marcos Schlick <marcosschlick@gmail.com>
2025-03-06 17:17:52 -0300

    Configuração inicial do projeto: adicionados requirements.txt, src/ e .gitignore

---
+++ src/test.py
@@ -0,0 +1,7 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+
+print("OpenCV version:", cv2.__version__)
+print("TensorFlow version:", tf.__version__)
+print("NumPy version:", np.__version__)
